18d8f677ab7c740cbb4d5843098e94aadf68cbc3
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-09-12 15:47:21 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -103,7 +103,7 @@ fi
 # fi
 sleep 2
 
-wget --no-check-certificate --no-cache --no-dns-cache 'https://github.com/Belfagor2005/LinuxsatPanel/archive/refs/heads/main.tar.gz'
+wget --no-check-certificate 'https://github.com/Belfagor2005/LinuxsatPanel/archive/refs/heads/main.tar.gz'
 tar -xzf main.tar.gz
 cp -r 'LinuxsatPanel-main/usr' '/'
 set +e
