4b1bed327cfc2c9a78adf8c14b37dbf9ce363930
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-10-27 14:37:06 +0100

    Update __init__.py

--- usr/lib/enigma2/python/Plugins/Extensions/LinuxsatPanel/addons/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/LinuxsatPanel/addons/__init__.py
@@ -25,39 +25,39 @@ __copyright__ = 'Copyright (c) 2024 Lululla'
 __license__ = "GPL-v2"
 __version__ = "1.0.0"
 
-# from Components.Language import language
-# from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-# import gettext
-# import os
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+import gettext
+import os
 
 
-# PluginLanguageDomain = 'LinuxsatPanel'
-# PluginLanguagePath = 'Extensions/LinuxsatPanel/locale'
+PluginLanguageDomain = 'LinuxsatPanel'
+PluginLanguagePath = 'Extensions/LinuxsatPanel/locale'
 
 
-# isDreambox = False
-# if os.path.exists("/usr/bin/apt-get"):
-    # isDreambox = True
+isDreambox = False
+if os.path.exists("/usr/bin/apt-get"):
+    isDreambox = True
 
 
-# def localeInit():
-    # if isDreambox:
-        # lang = language.getLanguage()[:2]
-        # os.environ["LANGUAGE"] = lang
-    # gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+def localeInit():
+    if isDreambox:
+        lang = language.getLanguage()[:2]
+        os.environ["LANGUAGE"] = lang
+    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
-# if isDreambox:
-    # def _(txt):
-        # return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
-# else:
-    # def _(txt):
-        # translated = gettext.dgettext(PluginLanguageDomain, txt)
-        # if translated:
-            # return translated
-        # else:
-            # print(("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)))
-            # return gettext.gettext(txt)
+if isDreambox:
+    def _(txt):
+        return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
+else:
+    def _(txt):
+        translated = gettext.dgettext(PluginLanguageDomain, txt)
+        if translated:
+            return translated
+        else:
+            print(("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)))
+            return gettext.gettext(txt)
 
-# localeInit()
-# language.addCallback(localeInit)
+localeInit()
+language.addCallback(localeInit)
