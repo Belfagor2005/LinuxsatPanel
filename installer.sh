e94e3b98ed619c0c7a3ba23d34b918d393fbc6ee
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-09 19:14:32 +0100

    2.5.7

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/LinuxsatPanel/main/installer.sh -O - | /bin/sh
 
 ## Only This 2 lines to edit with new version ######
-version='2.5.6'
-changelog='\nFix Move Pixmap\n'
+version='2.5.7'
+changelog='\nAdd Script - Pixmaps\n'
 
 ##############################################################
 
