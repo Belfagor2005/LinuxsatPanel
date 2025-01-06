a2e577f157060ec293f6baed7484da9073521aa7
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-01-06 19:21:36 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/LinuxsatPanel/main/installer.sh -O - | /bin/bash
+## setup command=wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/LinuxsatPanel/main/installer.sh -O - | /bin/bash
 
 ## Only This 2 lines to edit with new version ######
 version='2.6.7'
-changelog='\n--Removed Christmas Bot'
+changelog=(
+    "\n"
+    "-- Fix Arabic menulist\n"
+    "-- Fix Command utility_Get\n"
+    "-- Fix Screen\n"
+    "-- Fix Sort & Halign\n"
+    "-- Minor fix"
+)
 
 ##############################################################
 
