a591b4683c55d372cea8ded369c2f07e5050c34c
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-01-11 21:39:54 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ## setup command=wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/LinuxsatPanel/main/installer.sh -O - | /bin/bash
 
 ## Only This 2 lines to edit with new version ######
